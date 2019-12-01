859c66d11da04226a87300586e119a2e3dac8803
Tyler Zhang <t-veor@users.noreply.github.com>
2019-12-01 05:13:20 +0000

    Add Python solution for day 1

---
+++ 2019/day1/day1.py
@@ -0,0 +1,29 @@
+import sys
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = "input.txt"
+
+data = open("input.txt").read().splitlines()
+
+# star 1
+print(sum(int(i) // 3 - 2 for i in data))
+
+# star 2
+total = 0
+for i in data:
+    mass = int(i)
+    tot = 0
+    next_req = mass
+
+    while True:
+        next_req = next_req // 3 - 2
+        if next_req <= 0:
+            break
+        tot += next_req
+
+    total += tot
+
+print(total)
+
