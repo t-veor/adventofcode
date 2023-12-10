df03803a1f03ceea9d77a142ed1fb0ddc0185c71
Tyler Zhang <t-veor@users.noreply.github.com>
2023-12-10 15:10:13 +0000

    Learning about the shoelace formula and Pick's theorem

--- 2023/day10/src/main.rs
+++ 2023/day10/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::{HashMap, HashSet, VecDeque};
-
 use bitflags::bitflags;
 use utils::read_input_file;
 
@@ -63,7 +61,6 @@ fn connections(c: u8) -> Dir {
 
 struct PipeGrid {
     grid: Vec<Vec<u8>>,
-    size: (isize, isize),
     start_pos: (isize, isize),
     start_connections: Dir,
 }
@@ -71,9 +68,6 @@ struct PipeGrid {
 impl PipeGrid {
     fn parse(input: &str) -> Self {
         let grid: Vec<_> = input.lines().map(|line| line.as_bytes().to_vec()).collect();
-
-        let width = grid.first().map(|l| l.len()).unwrap_or(0);
-        let height = grid.len();
 
         let start_pos = grid
             .iter()
@@ -105,7 +99,6 @@ impl PipeGrid {
 
         Self {
             grid,
-            size: (width as isize, height as isize),
             start_pos,
             start_connections,
         }
@@ -133,97 +126,16 @@ impl PipeGrid {
             })
             .unwrap_or(Dir::empty())
     }
-
-    fn in_bounds(&self, (x, y): (isize, isize)) -> bool {
-        let (width, height) = self.size;
-        (0..width).contains(&x) && (0..height).contains(&y)
-    }
-
-    fn adjacent(&self, doubled: DoubledCoords) -> Vec<DoubledCoords> {
-        let mut result = Vec::with_capacity(4);
-
-        let connections = self.connections_at(doubled.to_coords());
-        let (qx, qy) = doubled.quadrant();
-
-        if qx == 1
-            || qy == 0 && !connections.contains(Dir::NORTH)
-            || qy == 1 && !connections.contains(Dir::SOUTH)
-        {
-            // east
-            result.push(DoubledCoords {
-                x: doubled.x + 1,
-                ..doubled
-            });
-        }
-
-        if qx == 0
-            || qy == 0 && !connections.contains(Dir::NORTH)
-            || qy == 1 && !connections.contains(Dir::SOUTH)
-        {
-            // west
-            result.push(DoubledCoords {
-                x: doubled.x - 1,
-                ..doubled
-            });
-        }
-
-        if qy == 1
-            || qx == 0 && !connections.contains(Dir::WEST)
-            || qx == 1 && !connections.contains(Dir::EAST)
-        {
-            // south
-            result.push(DoubledCoords {
-                y: doubled.y + 1,
-                ..doubled
-            })
-        }
-
-        if qy == 0
-            || qx == 0 && !connections.contains(Dir::WEST)
-            || qx == 1 && !connections.contains(Dir::EAST)
-        {
-            // north
-            result.push(DoubledCoords {
-                y: doubled.y - 1,
-                ..doubled
-            })
-        }
-
-        result
-    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct DoubledCoords {
-    x: isize,
-    y: isize,
-}
+fn find_path(grid: &PipeGrid) -> Vec<(isize, isize)> {
+    let mut path = vec![grid.start_pos];
 
-impl DoubledCoords {
-    fn from_coords(pos: (isize, isize), quadrant: (isize, isize)) -> Self {
-        Self {
-            x: 2 * pos.0 + quadrant.0,
-            y: 2 * pos.1 + quadrant.1,
-        }
-    }
-
-    fn to_coords(self) -> (isize, isize) {
-        (self.x.div_euclid(2), self.y.div_euclid(2))
-    }
-
-    fn quadrant(self) -> (isize, isize) {
-        (self.x.rem_euclid(2), self.y.rem_euclid(2))
-    }
-}
-
-fn star1(grid: &PipeGrid) -> i32 {
     let mut next_dir = grid.start_connections.pick();
     let mut pos = grid.start_pos;
-    let mut steps = 0;
     loop {
         let delta = next_dir.delta().unwrap();
         pos = (pos.0 + delta.0, pos.1 + delta.1);
-        steps += 1;
 
         let connections = grid.connections_at(pos);
         next_dir = connections.difference(next_dir.flip());
@@ -231,55 +143,33 @@ fn star1(grid: &PipeGrid) -> i32 {
         if pos == grid.start_pos {
             break;
         }
+
+        path.push(pos);
     }
 
-    steps / 2
+    path
 }
 
-fn find_enclosed_area(grid: &PipeGrid, start_pos: DoubledCoords) -> Option<usize> {
-    let mut seen = HashSet::new();
-    let mut frontier = VecDeque::new();
-
-    seen.insert(start_pos);
-    frontier.push_back(start_pos);
-
-    while let Some(pos) = frontier.pop_front() {
-        if !grid.in_bounds(pos.to_coords()) {
-            return None;
-        }
-
-        for adjacent in grid.adjacent(pos) {
-            if seen.insert(adjacent) {
-                frontier.push_back(adjacent);
-            }
-        }
-    }
-
-    // Count grid cells where all 4 quadrants are accessible.
-    let mut accessible_quadrant_count = HashMap::new();
-
-    for doubled_coords in seen {
-        let pos = doubled_coords.to_coords();
-        *accessible_quadrant_count.entry(pos).or_insert(0) += 1;
-    }
-
-    Some(
-        accessible_quadrant_count
-            .values()
-            .filter(|&&v| v >= 4)
-            .count(),
-    )
+fn star1(grid: &PipeGrid) -> usize {
+    find_path(grid).len() / 2
 }
 
-fn star2(grid: &PipeGrid) -> usize {
-    for quadrant in [(0, 0), (0, 1), (1, 0), (1, 1)] {
-        let start_pos = DoubledCoords::from_coords(grid.start_pos, quadrant);
-        if let Some(enclosed_area) = find_enclosed_area(grid, start_pos) {
-            return enclosed_area;
-        }
-    }
+fn shoelace_area(path: &[(isize, isize)]) -> isize {
+    (0..path.len())
+        .map(|i| (path[i], path[(i + 1) % path.len()]))
+        .map(|((x0, y0), (x1, y1))| x0 * y1 - x1 * y0)
+        .sum::<isize>()
+        .abs()
+        / 2
+}
 
-    panic!()
+fn star2(grid: &PipeGrid) -> isize {
+    let path = find_path(grid);
+    let boundary_points = path.len() as isize;
+    let enclosed_area = shoelace_area(&path);
+
+    // Pick's theorem
+    enclosed_area + 1 - boundary_points / 2
 }
 
 fn main() {
