b1f63abab832c520b60bd7e2d8ba2071222629e2
Tyler Zhang <t-veor@users.noreply.github.com>
2023-12-18 13:27:33 +0000

    Improve day 17 somewhat#

--- 2023/day17/src/main.rs
+++ 2023/day17/src/main.rs
@@ -1,101 +1,133 @@
 use utils::{pathfinding::dijkstra, read_input_file, Direction};
 
-fn parse_input(input: &str) -> Vec<Vec<u8>> {
-    input
-        .lines()
-        .map(|line| line.as_bytes().iter().map(|&c| c - b'0').collect())
-        .collect()
+#[derive(Debug)]
+struct Map {
+    width: isize,
+    height: isize,
+    data: Vec<Vec<u8>>,
 }
 
-fn star1(map: &[Vec<u8>]) -> i64 {
-    let width = map.first().map(|v| v.len()).unwrap_or(0) as isize;
-    let height = map.len() as isize;
+impl Map {
+    fn parse(input: &str) -> Self {
+        let data: Vec<Vec<_>> = input
+            .lines()
+            .map(|line| line.as_bytes().iter().map(|&c| c - b'0').collect())
+            .collect();
+        let width = data.first().map(|row| row.len()).unwrap_or(0) as isize;
+        let height = data.len() as isize;
 
-    dijkstra(
-        (0isize, 0isize, None::<Direction>, 0usize),
-        |&(x, y, _, _)| (x, y) == (width - 1, height - 1),
-        |&(x, y, prev_dir, spaces_in_dir)| {
-            let mut results = Vec::with_capacity(3);
+        Self {
+            width,
+            height,
+            data,
+        }
+    }
 
-            let possible_moves = if let Some(prev_dir) = prev_dir {
-                let mut moves = vec![(prev_dir.rotate_cw(), true), (prev_dir.rotate_ccw(), true)];
-                if spaces_in_dir < 3 {
-                    moves.push((prev_dir, false));
-                }
-                moves
-            } else {
-                Direction::ALL.iter().map(|&dir| (dir, true)).collect()
-            };
+    fn get(&self, pos: (isize, isize)) -> Option<i64> {
+        let (x, y) = pos;
+        if (0..self.width).contains(&x) && (0..self.height).contains(&y) {
+            Some(self.data[y as usize][x as usize] as i64)
+        } else {
+            None
+        }
+    }
+}
 
-            for (dir, is_turn) in possible_moves {
-                let (nx, ny) = dir.step((x, y));
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct CrucibleState {
+    pos: (isize, isize),
+    last_dir: Option<Direction>,
+}
 
-                if (0..width).contains(&nx) && (0..height).contains(&ny) {
-                    let cost = map[ny as usize][nx as usize] as i64;
-                    results.push((
-                        (
-                            nx,
-                            ny,
-                            Some(dir),
-                            if is_turn { 1 } else { spaces_in_dir + 1 },
-                        ),
-                        cost,
+impl CrucibleState {
+    fn expand_star1(&self, map: &Map) -> Vec<(i64, Self)> {
+        let mut expanded = Vec::with_capacity(6);
+
+        let dirs = match self.last_dir {
+            Some(dir) => vec![dir.rotate_cw(), dir.rotate_ccw()],
+            None => Direction::ALL.to_vec(),
+        };
+
+        for dir in dirs {
+            let mut movement_cost = 0;
+            for n in 1..=3 {
+                let new_pos = dir.step_n(self.pos, n);
+                if let Some(heat_loss) = map.get(new_pos) {
+                    movement_cost += heat_loss;
+                    expanded.push((
+                        movement_cost,
+                        Self {
+                            pos: new_pos,
+                            last_dir: Some(dir),
+                        },
                     ))
+                } else {
+                    break;
                 }
             }
+        }
 
-            results
+        expanded
+    }
+
+    fn expand_star2(&self, map: &Map) -> Vec<(i64, Self)> {
+        let mut expanded = Vec::with_capacity(14);
+
+        let dirs = match self.last_dir {
+            Some(dir) => vec![dir.rotate_cw(), dir.rotate_ccw()],
+            None => Direction::ALL.to_vec(),
+        };
+
+        for dir in dirs {
+            let mut movement_cost = 0;
+            for n in 1..=10 {
+                let new_pos = dir.step_n(self.pos, n);
+                if let Some(heat_loss) = map.get(new_pos) {
+                    movement_cost += heat_loss;
+                    if n >= 4 {
+                        expanded.push((
+                            movement_cost,
+                            Self {
+                                pos: new_pos,
+                                last_dir: Some(dir),
+                            },
+                        ))
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        expanded
+    }
+}
+
+fn star1(map: &Map) -> i64 {
+    let dest_pos = (map.width - 1, map.height - 1);
+
+    dijkstra(
+        CrucibleState {
+            pos: (0, 0),
+            last_dir: None,
         },
+        |state| state.pos == dest_pos,
+        |state| state.expand_star1(map),
     )
     .unwrap()
     .0
 }
 
-fn star2(map: &[Vec<u8>]) -> i64 {
-    let width = map.first().map(|v| v.len()).unwrap_or(0) as isize;
-    let height = map.len() as isize;
+fn star2(map: &Map) -> i64 {
+    let dest_pos = (map.width - 1, map.height - 1);
 
     dijkstra(
-        (0isize, 0isize, None::<Direction>, 0usize),
-        |&(x, y, _, spaces_in_dir)| (x, y) == (width - 1, height - 1) && spaces_in_dir >= 4,
-        |&(x, y, prev_dir, spaces_in_dir)| {
-            let mut results = Vec::with_capacity(3);
-
-            let possible_moves = if let Some(prev_dir) = prev_dir {
-                let mut moves = Vec::with_capacity(3);
-                if spaces_in_dir < 10 {
-                    moves.push((prev_dir, false));
-                }
-                if spaces_in_dir >= 4 {
-                    moves.extend_from_slice(&[
-                        (prev_dir.rotate_cw(), true),
-                        (prev_dir.rotate_ccw(), true),
-                    ]);
-                }
-                moves
-            } else {
-                Direction::ALL.iter().map(|&dir| (dir, true)).collect()
-            };
-
-            for (dir, is_turn) in possible_moves {
-                let (nx, ny) = dir.step((x, y));
-
-                if (0..width).contains(&nx) && (0..height).contains(&ny) {
-                    let cost = map[ny as usize][nx as usize] as i64;
-                    results.push((
-                        (
-                            nx,
-                            ny,
-                            Some(dir),
-                            if is_turn { 1 } else { spaces_in_dir + 1 },
-                        ),
-                        cost,
-                    ))
-                }
-            }
-
-            results
+        CrucibleState {
+            pos: (0, 0),
+            last_dir: None,
         },
+        |state| state.pos == dest_pos,
+        |state| state.expand_star2(map),
     )
     .unwrap()
     .0
@@ -103,7 +135,7 @@ fn star2(map: &[Vec<u8>]) -> i64 {
 
 fn main() {
     let input = read_input_file!();
-    let map = parse_input(&input);
+    let map = Map::parse(&input);
 
     println!("{}", star1(&map));
     println!("{}", star2(&map));
--- 2023/utils/src/pathfinding.rs
+++ 2023/utils/src/pathfinding.rs
@@ -1,75 +1,65 @@
 use std::cmp::Ordering;
+use std::collections::hash_map::Entry;
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::hash::Hash;
 
-pub fn dijkstra<S>(
+#[derive(Debug, Clone)]
+struct AStarQueueItem<S> {
+    state: S,
+    cost: i64,
+    priority: i64,
+}
+
+impl<S> PartialEq for AStarQueueItem<S> {
+    fn eq(&self, other: &Self) -> bool {
+        self.priority == other.priority
+    }
+}
+
+impl<S> Eq for AStarQueueItem<S> {}
+
+impl<S> Ord for AStarQueueItem<S> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Reversed ordering so that the max-heap binary-heap becomes a min-heap
+        other.priority.cmp(&self.priority)
+    }
+}
+
+impl<S> PartialOrd for AStarQueueItem<S> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug)]
+struct AStarRecord<S> {
+    come_from: S,
+    best_cost: i64,
+}
+
+pub fn a_star<S>(
     initial: S,
     is_goal: impl Fn(&S) -> bool,
-    expand: impl Fn(&S) -> Vec<(S, i64)>,
+    expand: impl Fn(&S) -> Vec<(i64, S)>,
+    heuristic: impl Fn(&S) -> i64,
 ) -> Option<(i64, Vec<S>)>
 where
     S: Clone + Eq + Hash,
 {
-    #[derive(Debug, Clone)]
-    struct DijkstraState<S> {
-        state: S,
-        cost: i64,
-    }
-
-    impl<S> PartialEq for DijkstraState<S> {
-        fn eq(&self, other: &Self) -> bool {
-            self.cost == other.cost
-        }
-    }
-
-    impl<S> Eq for DijkstraState<S> {}
-
-    impl<S> Ord for DijkstraState<S> {
-        fn cmp(&self, other: &Self) -> Ordering {
-            // Reversed ordering so that the max-heap binary-heap becomes a min-heap
-            other.cost.cmp(&self.cost)
-        }
-    }
-
-    impl<S> PartialOrd for DijkstraState<S> {
-        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-            Some(self.cmp(other))
-        }
-    }
-
-    #[derive(Debug)]
-    struct StateData<S> {
-        come_from: Option<S>,
-        best_cost: Option<i64>,
-    }
-
-    impl<S> Default for StateData<S> {
-        fn default() -> Self {
-            Self {
-                come_from: None,
-                best_cost: None,
-            }
-        }
-    }
-
     let mut expanded = HashSet::new();
-    let mut state_data: HashMap<S, StateData<S>> = HashMap::new();
+    let mut state_data: HashMap<S, AStarRecord<S>> = HashMap::new();
     let mut priority_queue = BinaryHeap::new();
 
-    state_data.insert(
-        initial.clone(),
-        StateData {
-            come_from: None,
-            best_cost: Some(0),
-        },
-    );
-    priority_queue.push(DijkstraState {
+    priority_queue.push(AStarQueueItem {
         state: initial,
         cost: 0,
+        // Technically this is wrong - but this entry gets immediately removed
+        // so we don't care that it has a dummy priority of 0
+        priority: 0,
     });
 
     let mut found_goal = None;
-    while let Some(DijkstraState { state, cost }) = priority_queue.pop() {
+    while let Some(AStarQueueItem { state, cost, .. }) = priority_queue.pop() {
         if !expanded.insert(state.clone()) {
             continue;
         }
@@ -79,19 +69,39 @@ where
             break;
         }
 
-        for (next_state, weight) in expand(&state) {
+        for (weight, next_state) in expand(&state) {
             let next_cost = cost + weight;
 
-            let next_state_entry = state_data.entry(next_state.clone()).or_default();
-            if next_state_entry.best_cost.is_none()
-                || next_cost < next_state_entry.best_cost.unwrap()
-            {
-                next_state_entry.best_cost = Some(next_cost);
-                next_state_entry.come_from = Some(state.clone());
+            let next_state_entry = state_data.entry(next_state.clone());
 
-                priority_queue.push(DijkstraState {
+            let better_path_found = match next_state_entry {
+                Entry::Occupied(mut entry) => {
+                    if next_cost < entry.get().best_cost {
+                        entry.insert(AStarRecord {
+                            come_from: state.clone(),
+                            best_cost: next_cost,
+                        });
+                        true
+                    } else {
+                        false
+                    }
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert(AStarRecord {
+                        come_from: state.clone(),
+                        best_cost: next_cost,
+                    });
+                    true
+                }
+            };
+
+            if better_path_found {
+                let h = heuristic(&next_state);
+
+                priority_queue.push(AStarQueueItem {
                     state: next_state,
                     cost: next_cost,
+                    priority: next_cost + h,
                 })
             }
         }
@@ -99,18 +109,29 @@ where
 
     found_goal.map(|state| {
         let mut path = Vec::new();
-        let entry = state_data.get(&state).unwrap();
+        let entry = state_data.get(&state);
 
         path.push(state);
 
-        let mut come_from = entry.come_from.clone();
+        let mut come_from = entry.map(|entry| entry.come_from.clone());
         while let Some(prev) = come_from {
-            let prev_entry = state_data.get(&prev).unwrap();
-            come_from = prev_entry.come_from.clone();
+            let prev_entry = state_data.get(&prev);
+            come_from = prev_entry.map(|entry| entry.come_from.clone());
             path.push(prev);
         }
 
         path.reverse();
-        (entry.best_cost.unwrap(), path)
+        (entry.map(|entry| entry.best_cost).unwrap_or(0), path)
     })
+}
+
+pub fn dijkstra<S>(
+    initial: S,
+    is_goal: impl Fn(&S) -> bool,
+    expand: impl Fn(&S) -> Vec<(i64, S)>,
+) -> Option<(i64, Vec<S>)>
+where
+    S: Clone + Eq + Hash,
+{
+    a_star(initial, is_goal, expand, |_| 0)
 }
